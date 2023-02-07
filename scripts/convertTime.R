621e07e0abb7ff1bbeb81a4136904e2452d0c8e0
Peter Humburg <peter.humburg@gmail.com>
2023-02-07 11:31:49 +1100

    initial commit

---
+++ scripts/convertTime.R
@@ -0,0 +1,21 @@
+library(lutz)
+library(purrr)
+library(lubridate)
+library(dplyr)
+library(readr)
+library(tidyr)
+
+eq_df <- read_csv("data/earthquatedates.csv")
+
+## Some entries are missing the minutes. I've replaced them with 0, i.e. start of the hour here
+eq_df <- eq_df %>% mutate(Mn = replace_na(Mn, 0))
+
+## Convert coordinates to time zones. Note that this uses current time zones, not historic ones
+eq_df <- eq_df %>% mutate(UTC=paste0(Year,"-", Mo, "-", Dy, " ", sprintf("%02d", Hr), ":", sprintf("%02d", Mn)),
+                          timezone=tz_lookup_coords(lat=eq_df$Latitude, lon=eq_df$Longitude, method="fast")) %>% 
+  mutate(local_time = map2(.x=UTC, .y=timezone, .f = function(x, y){
+    with_tz(time=as.POSIXct(x, format="%Y-%m-%d %H:%M", tz="UTC"), tzone=y)})) %>% unnest(local_time)
+
+
+map2(.x = date, .y = timezone, 
+     .f = function(x, y) {with_tz(time = x, tzone = y)})
