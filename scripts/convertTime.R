5d5f39402d8324ee1d0111e4a171fce3904b5929
Peter Humburg <peter.humburg@gmail.com>
2023-02-07 11:42:53 +1100

    Fix output formatting

--- scripts/convertTime.R
+++ scripts/convertTime.R
@@ -14,8 +14,7 @@ eq_df <- eq_df %>% mutate(Mn = replace_na(Mn, 0))
 eq_df <- eq_df %>% mutate(UTC=paste0(Year,"-", Mo, "-", Dy, " ", sprintf("%02d", Hr), ":", sprintf("%02d", Mn)),
                           timezone=tz_lookup_coords(lat=eq_df$Latitude, lon=eq_df$Longitude, method="fast")) %>% 
   mutate(local_time = map2(.x=UTC, .y=timezone, .f = function(x, y){
-    with_tz(time=as.POSIXct(x, format="%Y-%m-%d %H:%M", tz="UTC"), tzone=y)})) %>% unnest(local_time)
+    with_tz(time=as.POSIXct(x, format="%Y-%m-%d %H:%M", tz="UTC"), tzone=y)})) %>% 
+  unnest(local_time) %>% mutate(local_time = as.character(local_time))
 
-
-map2(.x = date, .y = timezone, 
-     .f = function(x, y) {with_tz(time = x, tzone = y)})
+write_csv(eq_df, file="data/earthquakedates_conversion.csv")
